ffb1703c786d9c71b51f54df339d3c233988acaa
Serhii Khoma <srghma@gmail.com>
2025-05-06 08:36:46 +0700

    fix: warnings

--- src/Record/ExtraSrghma.purs
+++ src/Record/ExtraSrghma.purs
@@ -3,32 +3,32 @@ module Record.ExtraSrghma
   ( module Export
   ) where
 
-import Record.ExtraSrghma.AppendRecord as Export
-import Record.ExtraSrghma.MapRecord as Export
-import Record.ExtraSrghma.SequenceRecord as Export
-import Record.ExtraSrghma.MapValuesWithIndex as Export
-import Record.ExtraSrghma.ParSequenceRecord as Export
-import Record.ExtraSrghma.Keys.NonEmptyArray as Export
-import Record.ExtraSrghma.Keys.List as Export
-import Record.ExtraSrghma.Keys.Pick as Export
-import Record.ExtraSrghma.Keys.Array as Export
-import Record.ExtraSrghma.Keys.NonEmptyList as Export
-import Record.ExtraSrghma.FoldrValuesLazy as Export
-import Record.ExtraSrghma.CompareRecord as Export
-import Record.ExtraSrghma.MapIndex as Export
-import Record.ExtraSrghma.Values.NonEmptyArray as Export
-import Record.ExtraSrghma.Values.List as Export
-import Record.ExtraSrghma.Values.Array as Export
-import Record.ExtraSrghma.Values.NonEmptyList as Export
-import Record.ExtraSrghma.SList.Types as Export
-import Record.ExtraSrghma.SList.NonEmptyArray as Export
-import Record.ExtraSrghma.SList.List as Export
-import Record.ExtraSrghma.SList.Array as Export
-import Record.ExtraSrghma.SList.NonEmptyList as Export
-import Record.ExtraSrghma.TraverseRecord as Export
-import Record.ExtraSrghma.FoldrValues as Export
-import Record.ExtraSrghma.ZipRecord as Export
-import Record.ExtraSrghma.FoldlValues as Export
-import Record.ExtraSrghma.ValuesToUnfoldableLazy as Export
-import Record.ExtraSrghma.FoldlValuesWithIndex as Export
-import Record.ExtraSrghma.FoldrValuesWithIndex as Export
+import Record.ExtraSrghma.AppendRecord (class AppendSubrecordImpl, appendRecord, appendSubrecordImpl) as Export
+import Record.ExtraSrghma.MapRecord (class MapRecord, mapRecord, mapRecordBuilder) as Export
+import Record.ExtraSrghma.SequenceRecord (class SequenceRecord, sequenceRecord, sequenceRecordImpl) as Export
+import Record.ExtraSrghma.MapValuesWithIndex (class MapValuesWithIndex, mapValuesWithIndex, mapValuesWithIndexBuilder) as Export
+import Record.ExtraSrghma.ParSequenceRecord (class ParSequenceRecord, parSequenceRecord, parSequenceRecordImpl) as Export
+import Record.ExtraSrghma.Keys.NonEmptyArray (class RLToNEAK, class RLToNEANEK, class RecordToNEAK, class RecordToNEANEK, recordToNEAK, recordToNEANEK, rlToNEAK, rlToNEANEK, rowToNEAK, rowToNEANEK) as Export
+import Record.ExtraSrghma.Keys.List (class RLToLK, class RLToLNEK, class RecordToLK, class RecordToLNEK, recordToLK, recordToLNEK, rlToLK, rlToLNEK, rowToLK, rowToLNEK) as Export
+import Record.ExtraSrghma.Keys.Pick (pick, pickFn) as Export
+import Record.ExtraSrghma.Keys.Array (class RLToAK, class RLToANEK, class RecordToAK, class RecordToANEK, recordToAK, recordToANEK, rlToAK, rlToANEK, rowToAK, rowToANEK) as Export
+import Record.ExtraSrghma.Keys.NonEmptyList (class RLToNELK, class RLToNELNEK, class RecordToNELK, class RecordToNELNEK, recordToNELK, recordToNELNEK, rlToNELK, rlToNELNEK, rowToNELK, rowToNELNEK) as Export
+import Record.ExtraSrghma.FoldrValuesLazy (class FoldrValuesLazy, class FoldrValuesLazy1, foldMapValuesLazyR, foldMapValuesLazyR1, foldrValuesLazy, foldrValuesLazy1, foldrValuesLazyImpl, foldrValuesLazyImpl1) as Export
+import Record.ExtraSrghma.CompareRecord (class OrdRecord, compareRecord, compareRecordImpl) as Export
+import Record.ExtraSrghma.MapIndex (class MapIndex, mapIndex, mapIndexBuilder) as Export
+import Record.ExtraSrghma.Values.NonEmptyArray (class RLToNEAV, recordToNEAV, rlToNEAV) as Export
+import Record.ExtraSrghma.Values.List (class RLToLV, recordToLV, rlToLV) as Export
+import Record.ExtraSrghma.Values.Array (class RLToAV, recordToAV, rlToAV) as Export
+import Record.ExtraSrghma.Values.NonEmptyList (class RLToNELV, recordToNELV, rlToNELV) as Export
+import Record.ExtraSrghma.SList.Types (class SListToRowList, type (:::), SCons, SList, SNil) as Export
+import Record.ExtraSrghma.SList.NonEmptyArray (class SListToNEANES, class SListToNEAS, slistToNEANES, slistToNEAS) as Export
+import Record.ExtraSrghma.SList.List (class SListToLNES, class SListToLS, slistToLNES, slistToLS) as Export
+import Record.ExtraSrghma.SList.Array (class SListToANES, class SListToAS, slistToANES, slistToAS) as Export
+import Record.ExtraSrghma.SList.NonEmptyList (class SListToNELNES, class SListToNELS, slistToNELNES, slistToNELS) as Export
+import Record.ExtraSrghma.TraverseRecord (class TraverseRecord, traverseRecord, traverseRecordImpl) as Export
+import Record.ExtraSrghma.FoldrValues (class FoldrValues, class FoldrValues1, foldMapValuesR, foldMapValuesR1, foldrValues, foldrValues1, foldrValuesImpl, foldrValuesImpl1) as Export
+import Record.ExtraSrghma.ZipRecord (class ZipRecord, zipRecord, zipRecordImpl) as Export
+import Record.ExtraSrghma.FoldlValues (class FoldlValues, class FoldlValues1, foldMapValuesL, foldMapValuesL1, foldlValues, foldlValues1, foldlValuesImpl, foldlValuesImpl1) as Export
+import Record.ExtraSrghma.ValuesToUnfoldableLazy (LazyTupleList(..), valuesToUnfoldableLazy) as Export
+import Record.ExtraSrghma.FoldlValuesWithIndex (class FoldlValuesWithIndex, class FoldlValuesWithIndex1, foldMapValuesWithIndexL, foldMapValuesWithIndexL1, foldlValuesWithIndex, foldlValuesWithIndex1, foldlValuesWithIndexImpl, foldlValuesWithIndexImpl1) as Export
+import Record.ExtraSrghma.FoldrValuesWithIndex (class FoldrValuesWithIndex, class FoldrValuesWithIndex1, foldMapValuesWithIndexR, foldMapValuesWithIndexR1, foldrValuesWithIndex, foldrValuesWithIndex1, foldrValuesWithIndexImpl, foldrValuesWithIndexImpl1) as Export
